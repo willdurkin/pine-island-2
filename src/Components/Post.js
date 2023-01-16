4bbd7fb8f7a38650c22e2e2e5820174a4e8fa899
Will Durkin <willdurkin@gmail.com>
2023-01-16 13:48:46 -0500

    Re-added code to Post.js

--- src/Components/Post.js
+++ src/Components/Post.js
@@ -1,19 +1,34 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
-
 const Post = () => {
 
+	const [postId, setPostId] = useEffect(2272)
+	const [post, setPost] = useEffect({})
 
+	useEffect(() => {
+	    let url =
+	      `https://pineisland.org/blog/wp-json/wp/v2/posts/${postId}`;
+
+	    const makeApiCall = async () => {
+	      const res = await fetch(url);
+	      const json = await res.json();
+
+	      	console.log("blogpost - ", json);
+	      	console.log("res ", res.headers);
+	      
+	      setPost(json);
+	    };
+	    makeApiCall();
+	  }, [setPost]);
 
 	return (
 		
 		<div className='blog'>
 			<p>This is a post!</p>
-
+			<div dangerouslySetInnerHTML={{ __html: post.title.rendered }} />
 		</div>
 	)
 };
-
 
 export default Post;
